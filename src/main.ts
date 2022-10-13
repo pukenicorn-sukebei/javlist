aa8dc52faebf0632bc71b8ca804a71f34d237de1
mhaii <mhaii@pukenicorn.com>
2022-10-13 13:21:36 +0700

    cleanup `main.ts`, handle security vulnerability (#26)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { ValidationPipe, VersioningType } from '@nestjs/common'
+import {
+  INestApplication,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import {
@@ -9,6 +13,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { useContainer } from 'class-validator'
 import * as DayJS from 'dayjs'
 import * as DayJS_UTC from 'dayjs/plugin/utc'
+import Fastify from 'fastify'
+import { FastifyInstance } from 'fastify/types/instance'
 
 import { IAppConfig } from '@_config/app.config'
 import { ISwaggerConfig } from '@_config/swagger.config'
@@ -20,9 +26,10 @@ import validationOptions from './utils/validation-options'
 DayJS.extend(DayJS_UTC)
 
 async function bootstrap() {
+  const fastify = getFastifyInstance()
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter(fastify),
     {
       cors: true,
     },
@@ -40,6 +47,36 @@ async function bootstrap() {
   // app.useGlobalInterceptors(new SerializerInterceptor())
   app.useGlobalPipes(new ValidationPipe(validationOptions))
 
+  setupSwagger(app, swaggerConfig)
+
+  await app.listen(appConfig.port, '0.0.0.0')
+}
+
+void bootstrap()
+
+function getFastifyInstance(): FastifyInstance {
+  const fastify = Fastify()
+
+  // https://hackerone.com/bugs?report_id=1715536&subject=fastify
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const badNames = Object.getOwnPropertyNames({}.__proto__)
+  fastify.addHook('onRequest', async (req, reply) => {
+    for (const badName of badNames) {
+      if (req.headers['content-type'].indexOf(badName) > -1) {
+        void reply.code(415)
+        throw new Error('Content type not supported')
+      }
+    }
+  })
+
+  return fastify
+}
+
+function setupSwagger(
+  app: INestApplication,
+  swaggerConfig: ISwaggerConfig,
+): void {
   if (swaggerConfig.enabled) {
     const options = new DocumentBuilder()
       .setTitle(swaggerConfig.title)
@@ -51,7 +88,4 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, options)
     SwaggerModule.setup(swaggerConfig.path, app, document)
   }
-
-  await app.listen(appConfig.port, '0.0.0.0')
 }
-void bootstrap()
