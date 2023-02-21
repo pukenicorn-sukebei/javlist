74a50ac126853f0f7df3e66214996f9dc8217906
mhaii <mhaii@pukenicorn.com>
2023-02-22 02:02:43 +0700

    add default for videokind

--- prisma/migrations/20230221183828_add_video_kind/migration.sql
+++ prisma/migrations/20230221183828_add_video_kind/migration.sql
@@ -1,11 +1,5 @@
-/*
-  Warnings:
-
-  - Added the required column `kind` to the `Video` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- CreateEnum
 CREATE TYPE "VideoKind" AS ENUM ('Jav');
 
 -- AlterTable
-ALTER TABLE "Video" ADD COLUMN     "kind" "VideoKind" NOT NULL;
+ALTER TABLE "Video" ADD COLUMN     "kind" "VideoKind" NOT NULL DEFAULT 'Jav';
