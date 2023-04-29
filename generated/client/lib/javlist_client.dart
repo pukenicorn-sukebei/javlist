3300b9c3b47a8efabd50ffd68f1b409c7ed0cf0e
Pukenicorn Service Account <service@pukenicorn.com>
2023-04-29 06:57:27 +0000

    Client Version: 1.1.8

---
+++ generated/client/lib/javlist_client.dart
@@ -0,0 +1,14 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+
+export 'package:javlist_client/src/api.dart';
+export 'package:javlist_client/src/auth/api_key_auth.dart';
+export 'package:javlist_client/src/auth/basic_auth.dart';
+export 'package:javlist_client/src/auth/oauth.dart';
+export 'package:javlist_client/src/serializers.dart';
+export 'package:javlist_client/src/model/date.dart';
+
+export 'package:javlist_client/src/api/videos_api.dart';
+
+export 'package:javlist_client/src/model/video_dto.dart';
