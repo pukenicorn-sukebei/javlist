2e492f7dc22189ea79cc46fd19e26c7f79c272d1
mhaii <mhaii@pukenicorn.com>
2023-05-06 02:10:21 +0700

    fix bool parser pipe logic (#186)

--- src/utils/pipes/parse-existence-bool.ts
+++ src/utils/pipes/parse-existence-bool.ts
@@ -1,10 +1,15 @@
 import { ArgumentMetadata, PipeTransform } from '@nestjs/common'
 
+import { parseTruthyFalsyString } from '@_utils/parsers'
+
 export class ParseExistenceBool implements PipeTransform<any, boolean> {
   transform(value: any, metadata: ArgumentMetadata): boolean {
     if (metadata.metatype !== Boolean) {
-      return value
+      throw new Error(
+        `Pipe used with incorrect type; Needed [Boolean], Got ${metadata.metatype}`,
+      )
     }
-    return value !== undefined
+
+    return parseTruthyFalsyString(value)
   }
 }
